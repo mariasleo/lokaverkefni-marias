5d4d221013c3a8099bcb7c9f5e2cef1f8413edf0
mariasleo <mariasleod@gmail.com>
2026-01-17 11:31:49 +0000

    Uppsetning á layouti

--- lokaverkefni-marias/src/App.jsx
+++ lokaverkefni-marias/src/App.jsx
@@ -28,8 +28,46 @@ export default function App() {
 
       <main>
         <div className="container">
-          <h1>Heim</h1>
-          <p>Meira kemur seinna</p>
+          <section className="hero">
+            <h1>Veistu ekki hvað á að vera í matinn?</h1>
+            <p className="subtitle">
+              Við hjálpum þér að finna það sem þig langar í
+            </p>
+
+            <div className="hero-actions">
+              <button className="btn">Uppskriftir</button>
+              <button className="btn secondary">Flokkar</button>
+            </div>
+          </section>
+
+          <section className="featured">
+            <div className="section-head">
+              <h2>Skemmtilegar hugmyndir</h2>
+              <a className="section-link" href="#">
+                Sýna allar →
+              </a>
+            </div>
+
+            <div className="card-grid">
+              <div className="card">
+                <div className="card-img"></div>
+                <h3>Rjóma Pasta</h3>
+                <p className="subtitle">Ready in 25 min</p>
+              </div>
+
+              <div className="card">
+                <div className="card-img"></div>
+                <h3>Fiskur</h3>
+                <p className="subtitle">Ready in 30 min</p>
+              </div>
+
+              <div className="card">
+                <div className="card-img"></div>
+                <h3>Kjúklingur</h3>
+                <p className="subtitle">Ready in 35 min</p>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
 
